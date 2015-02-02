fa07c909485445695a290e83693f36bc2d842a6e
Gabriel_Azevedo <gabriel.azevedo95@icloud.com>
2015-02-02 14:40:49 -0800

    Initial Commit

---
+++ InstagramFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramFeed
+//
+//  Created by Gabriel Borri de Azevedo on 2/2/15.
+//  Copyright (c) 2015 Gabriel Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstagramFeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstagramFeed
+//
+//  Created by Gabriel Borri de Azevedo on 2/2/15.
+//  Copyright (c) 2015 Gabriel Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
